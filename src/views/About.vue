26d360fdbac87d46cbc3674e0d24990582cba1a2
beilec <rushadaev@gmail.com>
2024-07-01 01:29:45 -0700

    Init commit

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Dj Connect</h1>
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
