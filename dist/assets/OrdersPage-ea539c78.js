340ecc31ca1b8fad30270b138805d1f5a1b7a8f0
denisaliev <alievdenis1@ya.ru>
2024-07-17 08:49:51 +0700

    delete dist

--- dist/assets/OrdersPage-ea539c78.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c}from"./index-e836614e.js";const s={},t={class:"text-red"};function o(n,_){return r(),c("div",t," orders ")}const d=e(s,[["render",o]]);export{d as default};
--- dist/assets/SupportPage-35adb8aa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c}from"./index-e836614e.js";const o={},r={class:"text-red"};function s(n,_){return t(),c("div",r," support ")}const p=e(o,[["render",s]]);export{p as default};
