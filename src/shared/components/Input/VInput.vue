ea62fcff8f2b5de8001143a74778aaca09b8c178
Pavel <tigerr.cat@gmail.com>
2024-07-30 23:34:47 +0300

    fix reg and edit 1

--- src/shared/components/Input/VInput.vue
+++ src/shared/components/Input/VInput.vue
@@ -2,14 +2,14 @@
 	<div class="v-input">
 		<label
 			:for="id"
-			class="block text-sm font-medium text-gray-700"
+			class="block text-sm font-medium text-gray-700 mb-[5px]"
 		>{{ label }}</label>
 		<div class="mt-1 relative rounded-md shadow-sm">
 			<input
 				:id="id"
 				v-bind="$attrs"
 				:value="modelValue"
-				class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-3 pr-12 sm:text-sm border-gray-300 rounded-md"
+				class="focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md p-3 border-custom"
 				:class="{ 'border-red-300': error }"
 				@input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
 			>
