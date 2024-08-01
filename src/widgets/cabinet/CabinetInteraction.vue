5a383978b03d3f2a78e768be705971456c715c12
denisaliev <alievdenis1@ya.ru>
2024-08-01 19:18:42 +0700

    правки

--- src/widgets/cabinet/CabinetInteraction.vue
+++ src/widgets/cabinet/CabinetInteraction.vue
@@ -15,29 +15,37 @@
 					Обновить QR
 				</VButton>
 			</div>
-			<div
-				v-else
-				class="absolute bottom-0 left-[50%] translate-x-[-50%] w-full p-[25px]"
-			>
-				<VButton class="flex gap-[10px] items-center">
-					<IconMusic icon-color="#131313" />
-					Заказать трек
-				</VButton>
-			</div>
+			<!--			<div-->
+			<!--				v-else-->
+			<!--				class="absolute bottom-0 left-[50%] translate-x-[-50%] w-full p-[25px]"-->
+			<!--			>-->
+			<!--				<VButton class="flex gap-[10px] items-center">-->
+			<!--					<IconMusic icon-color="#131313" />-->
+			<!--					Заказать трек-->
+			<!--				</VButton>-->
+			<!--			</div>-->
 		</div>
 		<div class="bg-blackContent p-[25px]">
 			<div class="flex justify-between">
 				<div class="flex flex-col gap-[5px]">
 					<span class="text-white text-xxl">{{ user?.is_dj ? 'DJ' : '' }} {{ user?.name }}</span>
-					<span>{{ user?.dj?.stage_name || 'Псевдоним' }}</span>
+					<div
+						v-if="user?.is_dj"
+						class="space-y-2 mt-2"
+					>
+						{{ djStore.tracks }}
+						<p><strong>Псевдоним:</strong> {{ user?.dj?.stage_name }}</p>
+						<p><strong>Город:</strong> {{ user?.dj?.city }}</p>
+						<p><strong>Базовая стоймость:</strong> {{ user?.dj?.price }}</p>
+					</div>
 				</div>
 				<div class="flex gap-[10px]">
 					<div
 						class="w-[48px] h-[48px] flex items-center justify-center rounded-full bg-lightGrey"
 					>
-						<router-link to='/'>
-							<IconGoogle
-								:icon-color="'#28B447'"
+						<router-link to="/">
+							<IconWorld
+								:icon-color="'white'"
 								class="w-[18px] h-[18px]"
 							/>
 						</router-link>
@@ -56,37 +64,10 @@
 					:photo="track.photo"
 				/>
 			</div>
-			<div class="bg-black text-white rounded-lg mt-[20px]">
-				<h2 class="text-lg font-bold mb-5">
-					{{ user?.is_dj ? 'Информация о DJ' : 'Стать DJ' }}
-				</h2>
-				<div
-					v-if="user?.is_dj"
-					class="space-y-4"
-				>
-					<p><strong>Город:</strong> {{ user.dj?.city }}</p>
-					<!-- <p><strong>Базовая цена:</strong> {{ user.dj?.price }}</p> -->
-				</div>
-
-				<h2 class="text-lg font-bold mb-5 mt-5">
-					Базовые стоимости на трек
-				</h2>
-				<div class="space-y-4">
-					<div class="flex gap-4">
-						<!-- <input
-							type="text"
-							placeholder="Название"
-							class="p-3 border-custom rounded-lg border border-gray text-white w-[65%]"
-						> -->
-						<input
-							type="text"
-							disabled
-							placeholder="Цена"
-							class="p-3 border-custom rounded-lg border border-gray text-white w-[10%]"
-							:value="user?.dj?.price"
-						>
-					</div>
-				</div>
+			<div
+				v-if="user?.is_dj"
+				class="text-white rounded-lg mt-[20px]"
+			>
 				<div v-if="user?.is_dj">
 					<VButton
 						class="mt-[20px] m-[auto]"
@@ -109,18 +90,18 @@
 						</span>
 					</VButton>
 				</div>
-				<div v-else>
-					<VButton
-						:color="ButtonColors.Green"
-						class="mt-[20px] m-[auto]"
-						@click="becomeDJ"
-					>
-						<span class="flex gap-[5px] items-center">
-							<IconMusic icon-color="#131313" />
-							Стать DJ
-						</span>
-					</VButton>
-				</div>
+			</div>
+			<div v-else>
+				<VButton
+					:color="ButtonColors.Green"
+					class="mt-[20px] m-[auto]"
+					@click="becomeDJ"
+				>
+					<span class="flex gap-[5px] items-center">
+						<IconMusic icon-color="#131313" />
+						Стать DJ
+					</span>
+				</VButton>
 			</div>
 		</div>
 	</div>
@@ -132,7 +113,7 @@ import { useRouter } from 'vue-router'
 import { useSessionStore } from '@/entities/session/model/session.store'
 import { useDJStore } from '@/entities/dj/model/dj.store'
 import { storeToRefs } from 'pinia'
-import { IconQr, IconGoogle, IconStat, IconEdit, IconMusic } from 'shared/components/Icon'
+import { IconQr, IconWorld, IconStat, IconEdit, IconMusic } from 'shared/components/Icon'
 import { VButton, ButtonColors } from 'shared/components/Button'
 import { VCard } from 'shared/components/Card'
 
@@ -144,30 +125,12 @@ const { user } = storeToRefs(sessionStore)
 
 // TODO: REPLACE WITH API
 const tracks = [
-    {
-        id: 1,
-        title: 'Песня 1',
-        text: 'Песня 1',
-        photo: '/public/cabinet_bg.png',
-    },
-    {
-        id: 2,
-        title: 'Песня 1',
-        text: 'Песня 1',
-        photo: '/public/cabinet_bg.png',
-    },
-    {
-        id: 3,
-        title: 'Песня 1',
-        text: 'Песня 1',
-        photo: '/public/cabinet_bg.png',
-    },
-    {
-        id: 4,
-        title: 'Песня 1',
-        text: 'Песня 1',
-        photo: '/public/cabinet_bg.png',
-    }
+  {
+    id: 1,
+    title: 'Песня 1',
+    text: 'Песня 1',
+    photo: '/public/cabinet_bg.png',
+  },
 ]
 
 onMounted(async () => {
