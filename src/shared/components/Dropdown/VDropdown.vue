98f16fe435b5c9e091f674835f079c09d2ab5149
Ivan Katkov <vanya6537@gmail.com>
2024-08-06 12:49:31 +0700

    feat(order): add dropdown to shared

---
+++ src/shared/components/Dropdown/VDropdown.vue
@@ -0,0 +1,84 @@
+<template>
+	<DropdownMenuRoot
+		v-model:open="toggleState"
+	>
+		<DropdownMenuTrigger>
+			{{ selectedTrackName || props.placeholder }}
+		</DropdownMenuTrigger>
+
+		<DropdownMenuPortal>
+			<DropdownMenuContent>
+				<DropdownMenuRadioGroup
+					v-model:modelValue="selectedValue"
+					class="min-w-[220px] outline-none rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+				>
+					<DropdownMenuRadioItem
+						v-for="option in props.options"
+						:key="option.value"
+						:value="option.value"
+						class="group text-[16px] leading-none rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none cursor-pointer"
+						@select="onSelect"
+					>
+						<DropdownMenuItemIndicator>
+							{{ props.indicator }}
+						</DropdownMenuItemIndicator>
+						{{ option.label }}
+						<div
+							class="ml-auto pl-[20px] text-lime-400 group-data-[highlighted]:text-white group-data-[disabled]:text-mauve8"
+						>
+							{{ option.additional }}
+						</div>
+					</DropdownMenuRadioItem>
+				</DropdownMenuRadioGroup>
+			</DropdownMenuContent>
+		</DropdownMenuPortal>
+	</DropdownMenuRoot>
+</template>
+
+<script setup lang="ts">
+import {
+  DropdownMenuContent,
+  DropdownMenuItemIndicator,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+  type DropdownMenuCheckboxItemEmits
+} from 'radix-vue'
+export type DropdownOption = { value: string; label: string, additional?:string };
+import { ref, watch } from 'vue'
+
+const toggleState = ref(false)
+
+const selectedValue = ref(null)
+const selectedTrackName = ref(null)
+// add reactive selectedTrackName to watch selectedValue
+
+const emit = defineEmits<DropdownMenuCheckboxItemEmits>()
+
+const onSelect = ( val: any ) => {
+  emit('select', val)
+}
+
+const props = withDefaults(
+  defineProps<{
+    options: DropdownOption[];
+    placeholder?: string;
+    indicator?: string;
+  }>(),
+  {
+    options: () => [] as DropdownOption[],
+    placeholder: 'Выберите трек',
+    indicator: '💿'
+  },
+)
+
+const onChange = () => {
+  emit('change')
+}
+
+watch(selectedValue, (value) => {
+  selectedTrackName.value = props.options.find((option) => option.value === value)?.label
+})
+</script>
