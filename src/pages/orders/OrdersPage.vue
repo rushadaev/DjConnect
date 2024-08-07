c11eac9d34094877febae932b91c7788dd2d04a0
Ivan Katkov <vanya6537@gmail.com>
2024-08-07 23:48:01 +0700

    feat(orders-page): orders page template

--- src/pages/orders/OrdersPage.vue
+++ src/pages/orders/OrdersPage.vue
@@ -1,6 +1,17 @@
 <template>
-	<div class="text-red">
-		orders
+	<div
+		class="flex items-center justify-center h-[100vh]"
+	>
+		<div class="px-6 pt-11 pb-4">
+			<div
+				class="flex flex-col justify-center items-center py-[170px] text-7xl"
+			>
+				<span>💿</span>
+				<h1 class="text-2xl pt-4">
+					Orders in progress..
+				</h1>
+			</div>
+		</div>
 	</div>
 </template>
 
