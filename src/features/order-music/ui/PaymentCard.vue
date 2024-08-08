8df9236296972d62294b76b7918efc630a329e38
Ivan Katkov <vanya6537@gmail.com>
2024-08-08 09:27:50 +0700

    feat(stats): implement PaymentsList, PaymentCard for FinancePage

---
+++ src/features/order-music/ui/PaymentCard.vue
@@ -0,0 +1,51 @@
+<template>
+	<button
+		class="flex w-full justify-between items-center p-[10px] h-[66px] bg-lightGrey rounded-[5px]"
+		:class="{'cursor-pointer': props?.routeParams}"
+		@click="goto"
+	>
+		<div class="flex gap-[10px]">
+			<div
+				class="w-[48px] h-[48px] rounded-[5px] flex flex-col justify-center items-center"
+			>
+				<span class="text-[27px]">📈</span>
+			</div>
+			<div class="flex flex-col justify-center">
+				<h3 class="text-white text-sm">
+					{{ title }}
+				</h3>
+				<p
+					class="text-xs"
+					:class="{ 'text-[#ADFF00]': props?.textColor === 'green',
+						'text-[#FFFFFF4D]': !props?.textColor
+					}"
+				>
+					{{ text }}
+				</p>
+			</div>
+		</div>
+		<div>
+			<slot name="status" />
+		</div>
+		<!-- </div> -->
+	</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const props = defineProps<{
+	photo: string
+	title: string
+	text: string
+	textColor?: string
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	routeParams?: any
+}>()
+// const publicPath = process.env.BASE_URL || '/DjConnect'
+const goto = () => {
+	if(props.routeParams) router.push(props.routeParams)
+}
+</script>
---
+++ src/features/order-music/ui/PaymentsList.vue
@@ -0,0 +1,68 @@
+<template>
+	<PaymentCard
+		v-for="item in props.items"
+		:key="item.id"
+		:photo="item.photo"
+		:title="item.title"
+		:text="item.text"
+		:route-params="item.routeParams"
+		:text-color="props?.textColor"
+		class="mb-[7px]"
+	>
+		<template
+			v-if="!!item.statusText"
+			#status
+		>
+			<VStatus :color="item.statusColor">
+				{{ item.statusText }}
+			</VStatus>
+		</template>
+	</PaymentCard>
+</template>
+
+<script setup lang="ts">
+import PaymentCard  from './PaymentCard.vue'
+import { VStatus } from '@/shared/components/Status'
+import { StatusVariable } from '@/shared/components/Status/config'
+
+const props = defineProps<{
+	textColor?: string,
+    items: {
+        id: number,
+        photo: string,
+        title: string,
+        text: string,
+        statusColor?: StatusVariable,
+        statusText?: string | 'Оплачено' | 'Ожидание' | 'Отменен'
+		routeParams?: { name: string , params: { id: number } }
+    }[]
+}>()
+// const items = ref([
+//     {
+//         id: 1,
+//         photo: 'cabinet_bg.png',
+//         title: 'Title 1',
+//         text: 'Text 1',
+//         statusColor: 'green' as StatusVariable,
+//         statusText: 'Оплачено'
+//     },
+//     {
+//         id: 2,
+//         photo: 'cabinet_bg.png',
+//         title: 'Title 2',
+//         text: 'Text 2',
+//         statusColor: 'orange' as StatusVariable,
+//         statusText: 'Ожидание'
+//     },
+//     {
+//         id: 2,
+//         photo: 'cabinet_bg.png',
+//         title: 'Title 2',
+//         text: 'Text 2',
+//         statusColor: 'red' as StatusVariable,
+//         statusText: 'Отменен'
+//     }
+// ])
+</script>
+
+<style scoped></style>
