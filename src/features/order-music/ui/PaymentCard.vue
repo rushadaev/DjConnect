04af3647c4f414d253d6e78a830b317814685496
Ivan Katkov <vanya6537@gmail.com>
2024-08-15 14:51:00 +0700

    feat(global): change main active color

--- src/features/order-music/ui/PaymentCard.vue
+++ src/features/order-music/ui/PaymentCard.vue
@@ -16,7 +16,7 @@
 				</h3>
 				<p
 					class="text-xs"
-					:class="{ 'text-[#ADFF00]': props?.textColor === 'green',
+					:class="{ 'text-[#F5E02C]': props?.textColor === 'green',
 						'text-[#FFFFFF4D]': !props?.textColor
 					}"
 				>
--- src/shared/components/ui/tabs/TabsTrigger.vue
+++ src/shared/components/ui/tabs/TabsTrigger.vue
@@ -2,7 +2,7 @@
 	<TabsTrigger
 		v-bind="forwardedProps"
 		:class="cn(
-			'w-[100%] inline-flex items-center  justify-center whitespace-nowrap rounded-[5px] px-[8px] py-[8px] text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#ADFF00] data-[state=active]:text-[#131313] data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50',
+			'w-[100%] inline-flex items-center  justify-center whitespace-nowrap rounded-[5px] px-[8px] py-[8px] text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#F5E02C] data-[state=active]:text-[#131313] data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50',
 			props.class,
 		)"
 	>
